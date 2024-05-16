ea896244a1c230b6af8827229503fafb24d68eaa
Udeme98 <Darealudeme1@gmail.com>
2024-05-16 12:27:22 +0100

    project-2: qr-code-generator

--- src/projects/qr-code-generator/index.jsx
+++ src/projects/qr-code-generator/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import QRCode from "react-qr-code";
+import "./styles.css";
 
 const QRCodeGenerator = () => {
   const [qrCode, setQrCode] = useState("");
@@ -9,23 +10,31 @@ const QRCodeGenerator = () => {
     setQrCode(input);
     setInput("");
   };
+
   return (
     <>
       <h2>QR Code Generator</h2>
-      <input
-        name="qr-code"
-        placeholder="Enter your value here.."
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button
-        onClick={handleGenerateQrCode}
-        disabled={input && input.trim() !== "" ? false : true}
-      >
-        Generate QR Code
-      </button>
 
-      <div>
-        <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+      <div className="qr-code">
+        <div>
+          <input
+            name="qr-code"
+            value={input}
+            placeholder="Enter your value here.."
+            onChange={(e) => setInput(e.target.value)}
+            style={{ marginRight: "5px" }}
+          />
+          <button
+            onClick={handleGenerateQrCode}
+            disabled={input && input.trim() !== "" ? false : true}
+          >
+            Generate QR Code
+          </button>
+        </div>
+
+        <div>
+          <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />
+        </div>
       </div>
     </>
   );
