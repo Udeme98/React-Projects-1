9e224fbb08a4e58d2b00bf370285809ec0e00151
Udeme98 <Darealudeme1@gmail.com>
2024-05-16 11:22:10 +0100

    project-1: accordion

--- src/projects/accordion/index.jsx
+++ src/projects/accordion/index.jsx
@@ -1,4 +1,35 @@
+import { useState } from "react";
+import data from "./data";
+import "./accordion.css";
+
 const Accordion = () => {
-  return <div>Accordion</div>;
+  const [selected, setSelected] = useState(null);
+
+  const handleSingle = (getSingleId) => {
+    // console.log(getSingleId);
+    setSelected(getSingleId === selected ? null : getSingleId);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div className="title" onClick={() => handleSingle(dataItem.id)}>
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ? (
+                <p className="content">{dataItem.answer}</p>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default Accordion;
